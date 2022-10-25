ea7a1268e177bad0a643e84e1584b9f4e51a52de
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-25 18:13:56 +0200

    Arithmentic broken sequence finder tests and implementation

---
+++ src/SmartGuideTdd.FirstNonConsecutiveNumber/ArithmeticBrokenSequenceFinder.cs
@@ -0,0 +1,30 @@
+﻿using SmartGuideTdd.FirstNonConsecutiveNumber.Abstraction;
+
+namespace SmartGuideTdd.FirstNonConsecutiveNumber
+{
+    public class ArithmeticBrokenSequenceFinder : IBrokenSequenceFinder
+    {
+        public int? Find(int[] sequence)
+        {
+            if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence));
+            }
+
+            if(sequence.Length < 2)
+            {
+                throw new ArgumentException(nameof(sequence), $"Parameter {nameof(sequence)} contains less than allowed minimum items. Minimum numer of allowed items is 2.");
+            }
+
+            for(int i = 1; i < sequence.Length; i++)
+            {
+                if ((sequence[i - 1] + 1) != sequence[i])
+                {
+                    return sequence[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
