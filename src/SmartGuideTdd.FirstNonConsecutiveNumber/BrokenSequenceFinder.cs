fbf53936833d4cb9ce939f64d64499cac1a3b620
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-25 17:14:40 +0200

    Added BrokenSequenceFinder without implementation

---
+++ src/SmartGuideTdd.FirstNonConsecutiveNumber/BrokenSequenceFinder.cs
@@ -0,0 +1,12 @@
+﻿using SmartGuideTdd.FirstNonConsecutiveNumber.Abstraction;
+
+namespace SmartGuideTdd.FirstNonConsecutiveNumber
+{
+    public class BrokenSequenceFinder : IBrokenSequenceFinder
+    {
+        public int Find(int[] sequence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
