8bd2170fc160ee3576c877fa9336ca3206eaa942
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-25 18:24:19 +0200

    LINQ intersect broken sequence finder tests and implementation

---
+++ src/SmartGuideTdd.FirstNonConsecutiveNumber/LinqIntersectBrokenSequenceFinder.cs
@@ -0,0 +1,29 @@
+﻿using SmartGuideTdd.FirstNonConsecutiveNumber.Abstraction;
+
+namespace SmartGuideTdd.FirstNonConsecutiveNumber
+{
+    public class LinqIntersectBrokenSequenceFinder : IBrokenSequenceFinder
+    {
+        public int? Find(int[] sequence)
+        {
+            if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence));
+            }
+
+            if(sequence.Length < 2)
+            {
+                throw new ArgumentException(nameof(sequence), $"Parameter {nameof(sequence)} contains less than allowed minimum items. Minimum numer of allowed items is 2.");
+            }
+
+            var range = Enumerable.Range(sequence[0], sequence.Length);
+
+            var result = sequence
+                .Intersect(range);
+
+            return result.Count() == 0
+                ? null
+                : result.First();
+        }
+    }
+}
---
+++ tests/SmartGuideTdd.FirstNonConsecutiveNumber.Tests/LinqIntersectBrokenSequenceFinderTests.cs
@@ -0,0 +1,70 @@
+namespace SmartGuideTdd.FirstNonConsecutiveNumber.Tests
+{
+    [TestFixture]
+    public class LinqIntersectBrokenSequenceFinderTests
+    {
+        [Test]
+        public void Array_Is_Null_Throws_ArgumentNullException()
+        {
+            var finder = new LinqIntersectBrokenSequenceFinder();
+
+            var action = () => finder.Find(null);
+
+            Assert.Throws<ArgumentNullException>(() => action());
+        }
+
+        [TestCase(new int[0])]
+        [TestCase(new[] { Int32.MinValue })]
+        [TestCase(new[] { 0 })]
+        [TestCase(new[] { Int32.MaxValue })]
+        public void Array_Has_Less_Than_Two_Items_Throws_ArgumentException(int[] sequence)
+        {
+            var finder = new ArithmeticBrokenSequenceFinder();
+
+            var action = () => finder.Find(sequence);
+
+            Assert.Throws<ArgumentException>(() => action());
+        }
+
+        //[TestCase(new[] { Int32.MaxValue, Int32.MinValue })]
+        //[TestCase(new[] { 0, Int32.MinValue })]
+        //[TestCase(new[] { Int32.MaxValue, 0 })]
+        //public void Array_Has_Unordered_Items_Throws_InvalidOperationException(int[] sequence)
+        //{
+        //    var finder = new ArithmeticBrokenSequenceFinder();
+
+        //    var action = () => finder.Find(sequence);
+
+        //    Assert.Throws<InvalidOperationException>(() => action());
+        //}
+
+        //[TestCase(new[] { Int32.MinValue, Int32.MinValue, 0, Int32.MaxValue })]
+        //[TestCase(new[] { Int32.MinValue, 0, 0, Int32.MaxValue })]
+        //[TestCase(new[] { Int32.MinValue, 0, Int32.MaxValue, Int32.MaxValue })]
+        //public void Array_Has_Duplicate_Items_Throws_InvalidOperationException(int[] sequence)
+        //{
+        //    var finder = new ArithmeticBrokenSequenceFinder();
+
+        //    var action = () => finder.Find(sequence);
+
+        //    Assert.Throws<InvalidOperationException>(() => action());
+        //}
+
+        [TestCase(new[] { 0, 1, 2 }, null)]
+        [TestCase(new[] { -2, -1, 0 }, null)]
+        [TestCase(new[] { -1, 0, 1 }, null)]
+        [TestCase(new[] { Int32.MinValue, Int32.MinValue + 1, Int32.MinValue + 2 }, null)]
+        [TestCase(new[] { Int32.MaxValue - 2, Int32.MaxValue - 1, Int32.MaxValue}, null)]
+        [TestCase(new[] { Int32.MinValue, Int32.MinValue + 2, Int32.MinValue + 3 }, Int32.MinValue + 2)]
+        [TestCase(new[] { Int32.MinValue, 0, 1 }, 0)]
+        [TestCase(new[] { 0, 1, Int32.MaxValue }, Int32.MaxValue)]
+        public void Array_Contains_Valid_Items_Returns_Null_Or_Number(int[] sequence, int? expected)
+        {
+            var finder = new ArithmeticBrokenSequenceFinder();
+
+            var actual = finder.Find(sequence);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
