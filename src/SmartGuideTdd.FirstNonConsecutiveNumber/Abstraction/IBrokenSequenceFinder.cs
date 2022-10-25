391eb704113bb8522623fd39e8f9a2254c87f5dc
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-25 17:12:37 +0200

    Added implementation IbBrokenSequnceFinder interface

---
+++ src/SmartGuideTdd.FirstNonConsecutiveNumber/Abstraction/IBrokenSequenceFinder.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartGuideTdd.FirstNonConsecutiveNumber.Abstraction
+{
+    /// <summary>
+    /// Provides method for finding first number that is missing in the sequence
+    /// </summary>
+    public interface IBrokenSequenceFinder
+    {
+        /// <summary>
+        /// Returns first number missing in <paramref name="sequence"/> sequence
+        /// </summary>
+        /// <param name="sequence">Array of number representing sequence</param>
+        /// <returns>Returns first number missing in the sequence, if any exists, otherwise null</returns>
+        public int Find(int[] sequence);
+    }
+}
