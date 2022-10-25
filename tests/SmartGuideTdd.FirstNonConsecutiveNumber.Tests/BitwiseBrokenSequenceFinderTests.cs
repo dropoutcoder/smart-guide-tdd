1ca45bb83fa71625f30fce800ad5b2deeed5af0c
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-25 18:07:18 +0200

    Renamed BrokenSequenceFinderTests to BitwiseBrokenSequenceFinderTests

--- tests/SmartGuideTdd.FirstNonConsecutiveNumber.Tests/BitwiseBrokenSequenceFinderTests.cs
+++ tests/SmartGuideTdd.FirstNonConsecutiveNumber.Tests/BitwiseBrokenSequenceFinderTests.cs
@@ -1,12 +1,12 @@
 namespace SmartGuideTdd.FirstNonConsecutiveNumber.Tests
 {
     [TestFixture]
-    public class BrokenSequenceFinderTests
+    public class BitwiseBrokenSequenceFinderTests
     {
         [Test]
         public void Array_Is_Null_Throws_ArgumentNullException()
         {
-            var finder = new BrokenSequenceFinder();
+            var finder = new BitwiseBrokenSequenceFinder();
 
             var action = () => finder.Find(null);
 
@@ -19,7 +19,7 @@ namespace SmartGuideTdd.FirstNonConsecutiveNumber.Tests
         [TestCase(new[] { Int32.MaxValue })]
         public void Array_Has_Less_Than_Two_Items_Throws_ArgumentException(int[] sequence)
         {
-            var finder = new BrokenSequenceFinder();
+            var finder = new BitwiseBrokenSequenceFinder();
 
             var action = () => finder.Find(sequence);
 
@@ -49,5 +49,22 @@ namespace SmartGuideTdd.FirstNonConsecutiveNumber.Tests
 
         //    Assert.Throws<InvalidOperationException>(() => action());
         //}
+
+        [TestCase(new[] { 0, 1, 2 }, null)]
+        [TestCase(new[] { -2, -1, 0 }, null)]
+        [TestCase(new[] { -1, 0, 1 }, null)]
+        [TestCase(new[] { Int32.MinValue, Int32.MinValue + 1, Int32.MinValue + 2 }, null)]
+        [TestCase(new[] { Int32.MaxValue - 2, Int32.MaxValue - 1, Int32.MaxValue}, null)]
+        [TestCase(new[] { Int32.MinValue, Int32.MinValue + 2, Int32.MinValue + 3 }, Int32.MinValue + 2)]
+        [TestCase(new[] { Int32.MinValue, 0, 1 }, 0)]
+        [TestCase(new[] { 0, 1, Int32.MaxValue }, Int32.MaxValue)]
+        public void Array_Contains_Valid_Items_Returns_Null_Or_Number(int[] sequence, int? expected)
+        {
+            var finder = new BitwiseBrokenSequenceFinder();
+
+            var actual = finder.Find(sequence);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
