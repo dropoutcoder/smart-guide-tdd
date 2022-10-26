9b97704470c4ab4c954e809a9c6f4f25ac2ff8bf
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-26 14:47:07 +0200

    Implemented sum function and tests

--- src/SmartGuideTdd.SumWithoutHighestAndLowest/ArraySummarizer.cs
+++ src/SmartGuideTdd.SumWithoutHighestAndLowest/ArraySummarizer.cs
@@ -2,8 +2,10 @@
 
 namespace SmartGuideTdd.SumWithoutHighestAndLowest
 {
+    /// <inheritdoc />
     public class ArraySummarizer : IArraySummarizer
     {
+        /// <inheritdoc />
         public int Sum(int[] array)
         {
             if (array is null)
@@ -11,7 +13,102 @@ namespace SmartGuideTdd.SumWithoutHighestAndLowest
                 throw new ArgumentNullException(nameof(array));
             }
 
-            throw new NotImplementedException();
+            var min = new Occurence();
+            var max = new Occurence();
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                TryUpdate(array[i], (o, v) => v < o.Value, ref min);
+                TryUpdate(array[i], (o, v) => v > o.Value, ref max);
+
+                checked
+                {
+                    sum += array[i];
+                }
+            }
+
+            return sum - min - max;
+        }
+
+        /// <summary>
+        /// Attempting to update existing value
+        /// </summary>
+        /// <param name="value">A value for occurence</param>
+        /// <param name="createNew">Function efaluating if new instance of <see cref="Occurence"/> should be created.</param>
+        /// <param name="occurence">Current occurence instance</param>
+        /// <returns>True if <paramref name="min"/> is updated, false if <paramref name="occurence"/> is replaced by new instance, otherwise false.</returns>
+        private static bool? TryUpdate(int value, Func<Occurence, int, bool> createNew, ref Occurence occurence)
+        {
+            if (occurence.Value == value)
+            {
+                occurence++;
+                return true;
+            }
+            
+            if (createNew(occurence, value))
+            {
+                occurence = new Occurence(value, 1);
+                return false;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Represents number of occurence for particular number
+        /// </summary>
+        public struct Occurence
+        {
+            /// <summary>
+            /// Initializes instance of Occurence
+            /// </summary>
+            /// <param name="value">Value for capturing occurences</param>
+            /// <param name="count">Number of initial occurences</param>
+            public Occurence(int value, int count)
+            {
+                Value = value;
+                Count = count;
+            }
+
+            public int Value { get; } = 0;
+            public int Count { get; private set; } = 0;
+
+            public int GetTotal()
+            {
+                var total = 0;
+
+                for (int i = 0; i < Count; i++)
+                {
+                    total += Value;
+                }
+
+                return total;
+            }
+
+            public static int operator +(int value, Occurence occurence)
+            {
+                return value + occurence.GetTotal();
+            }
+
+            public static int operator -(int value, Occurence occurence)
+            {
+                return value - occurence.GetTotal();
+            }
+
+            public static Occurence operator ++(Occurence value)
+            {
+                value.Count += 1;
+
+                return value;
+            }
+
+            public static Occurence operator --(Occurence value)
+            {
+                value.Count -= 1;
+
+                return value;
+            }
         }
     }
 }
--- tests/SmartGuideTdd.SumWithoutHighestAndLowest.Tests/ArraySummarizerTests.cs
+++ tests/SmartGuideTdd.SumWithoutHighestAndLowest.Tests/ArraySummarizerTests.cs
@@ -14,5 +14,28 @@ namespace SmartGuideTdd.SumWithoutHighestAndLowest.Tests
 
             Assert.Throws<ArgumentNullException>(() => action());
         }
+
+        [Test]
+        public void Array_Sum_Overflows_Throws_OverflowException()
+        {
+            var summarizer = new ArraySummarizer();
+
+            var action = () => summarizer.Sum(new int[] { Int32.MaxValue, Int32.MaxValue - 1, Int32.MaxValue - 2, Int32.MaxValue - 3, Int32.MaxValue - 4 });
+
+            Assert.Throws<OverflowException>(() => action());
+        }
+
+        [TestCase(new int[] { -10, 0, -10, 10, 9, 10 }, 9)]
+        [TestCase(new int[] { -23, 5, 5, -1, 11, 9, 10 }, 28)]
+        [TestCase(new int[] { 121, -9, 122, 81, 15 }, 217)]
+        [TestCase(new int[] { 0, 1, -1, -10, 10 }, 0)]
+        public void Array_Is_Null_Throws_ArgumentNullException(int[] value, int expected)
+        {
+            var summarizer = new ArraySummarizer();
+
+            var actual = summarizer.Sum(value);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
