1ce540c4113f9261cfad15d3e5dd90902bfeb57f
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-26 11:21:39 +0200

    Added SmartGuideTdd.FizzBuzz project with initial interface abstraction and not implemented class

---
+++ src/SmartGuideTdd.FizzBuzz/Abstraction/IFizzBuzzer.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartGuideTdd.FizzBuzz.Abstraction
+{
+    /// <summary>
+    /// Provides method evaluating if number is divisible by fizzbuzz numbers
+    /// </summary>
+    public interface IFizzBuzzer
+    {
+        /// <summary>
+        /// Evaluates if <paramref name="value"/> is dividible by 3, 5, or 15
+        /// </summary>
+        /// <param name="value">A positive number to evaluate</param>
+        /// <returns>Returns "fizz" for <paramref name="value"/> divisible by 3, "buzz" for number divisible by 5, "fizzbuzz" for number divisible by 15, otherwise returns <paramref name="value"/> as string</returns>
+        public string FizzBuzz(int value);
+    }
+}
---
+++ src/SmartGuideTdd.FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,14 @@
+﻿using SmartGuideTdd.FizzBuzz.Abstraction;
+
+namespace SmartGuideTdd.FizzBuzz
+{
+    /// <inheritdoc />
+    public class FizzBuzzer : IFizzBuzzer
+    {
+        /// <inheritdoc />
+        public string FizzBuzz(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
