2e83a7b7ba13b158d1d73d1bdfc894f88982c6f8
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-26 18:17:17 +0200

    Added project GameOfLife with implementation and test.

---
+++ src/SmartGuideTdd.GameOfLife/GameOfLife.cs
@@ -0,0 +1,122 @@
+﻿namespace SmartGuideTdd.GameOfLife
+{
+    public static class GameOfLife
+    {
+        public static int[,] Tick(ref int[,] current)
+        {
+            var m = current.GetLength(0);
+            var n = current.GetLength(1);
+
+            var next = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    next[i, j] = Evaluate(ref current, ref i, ref j, GetAvailableDirections(i, j, m, n));
+                }
+            }
+
+            return next;
+        }
+
+        private static int Evaluate(ref int[,] current, ref int horizontalIndex, ref int verticalIndex, Directions directions)
+        {
+            var cell = current[horizontalIndex, verticalIndex];
+
+            int aliveNeighbourCount = GetAliveNeighborCount(ref current, ref horizontalIndex, ref verticalIndex, ref directions);
+
+            if (cell == 0)
+            {
+                return Convert.ToInt32(aliveNeighbourCount == 3);
+            }
+            else
+            {
+                return Convert.ToInt32(aliveNeighbourCount >= 2 && aliveNeighbourCount <= 3);
+            }
+        }
+
+        private static int GetAliveNeighborCount(ref int[,] current, ref int horizontalIndex, ref int verticalIndex, ref Directions directions)
+        {
+            var aliveNeighbourCount = 0;
+
+            if (directions.HasFlag(Directions.Left))
+            {
+                aliveNeighbourCount += current[horizontalIndex, verticalIndex - 1];
+            }
+
+            if (directions.HasFlag(Directions.Left) && directions.HasFlag(Directions.Top))
+            {
+                aliveNeighbourCount += current[horizontalIndex - 1, verticalIndex - 1];
+            }
+
+            if (directions.HasFlag(Directions.Top))
+            {
+                aliveNeighbourCount += current[horizontalIndex - 1, verticalIndex];
+            }
+
+            if (directions.HasFlag(Directions.Top) && directions.HasFlag(Directions.Right))
+            {
+                aliveNeighbourCount += current[horizontalIndex - 1, verticalIndex + 1];
+            }
+
+            if (directions.HasFlag(Directions.Right))
+            {
+                aliveNeighbourCount += current[horizontalIndex, verticalIndex + 1];
+            }
+
+            if (directions.HasFlag(Directions.Right) && directions.HasFlag(Directions.Bottom))
+            {
+                aliveNeighbourCount += current[horizontalIndex + 1, verticalIndex + 1];
+            }
+
+            if (directions.HasFlag(Directions.Bottom))
+            {
+                aliveNeighbourCount += current[horizontalIndex + 1, verticalIndex];
+            }
+
+            if (directions.HasFlag(Directions.Bottom) && directions.HasFlag(Directions.Left))
+            {
+                aliveNeighbourCount += current[horizontalIndex + 1, verticalIndex - 1];
+            }
+
+            return aliveNeighbourCount;
+        }
+
+        private static Directions GetAvailableDirections(int horizontalIndex, int verticalIndex, int horizontalMax, int verticalMax)
+        {
+            var directions = Directions.Left | Directions.Top | Directions.Right | Directions.Bottom;
+
+            if (verticalIndex == 0)
+            {
+                directions &= ~Directions.Left;
+            }
+
+            if (horizontalIndex == 0)
+            {
+                directions &= ~Directions.Top;
+            }
+
+            if (verticalIndex == verticalMax - 1)
+            {
+                directions &= ~Directions.Right;
+            }
+            
+            if (horizontalIndex == horizontalMax - 1)
+            {
+                directions &= ~Directions.Bottom;
+            }
+
+            return directions;
+        }
+
+        [Flags]
+        private enum Directions
+        {
+            Left = 1,
+            Top = 2,
+            Right = 4,
+            Bottom = 8
+        }
+    }
+}
---
+++ tests/SmartGuideTdd.GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,48 @@
+namespace SmartGuideTdd.GameOfLife.Tests
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+        [Test]
+        [TestCaseSource(nameof(GetIt))]
+        public void Test1((int[,] Currect, int[,] Next) value)
+        {
+            WriteFormatted(value.Currect);
+
+            var next = GameOfLife.Tick(ref value.Currect);
+
+            WriteFormatted(next);
+
+            Assert.That(next, Is.EqualTo(value.Next));
+        }
+
+        private static IEnumerable<(int[,], int[,])> GetIt()
+        {
+            yield return (
+                new int[,] { { 0, 1, 1, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 1 }, { 0, 0, 0, 0 } },
+                new int[,] { { 0, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } }
+            );
+            yield return (
+                new int[,] { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } },
+                new int[,] { { 1, 0, 0, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 0, 0, 1 } }
+            );
+            yield return (
+                new int[,] { { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } },
+                new int[,] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } }
+            );
+        }
+
+        private void WriteFormatted(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
