20816adb45330df19e6a5432609b70f321a4fa73
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-27 12:04:46 +0200

    Added code docs for GameOfLife class

--- src/SmartGuideTdd.GameOfLife/GameOfLife.cs
+++ src/SmartGuideTdd.GameOfLife/GameOfLife.cs
@@ -1,7 +1,15 @@
 ﻿namespace SmartGuideTdd.GameOfLife
 {
+    /// <summary>
+    /// Provides method executing life evolution simulation
+    /// </summary>
     public class GameOfLife
     {
+        /// <summary>
+        /// Calculates next life evolution state
+        /// </summary>
+        /// <param name="current">Current life evolution state</param>
+        /// <returns>Calculated next life state based on current state</returns>
         public int[,] Tick(ref int[,] current)
         {
             var m = current.GetLength(0);
@@ -20,6 +28,14 @@
             return next;
         }
 
+        /// <summary>
+        /// Evaluates next cell life evolution state based one current neighbor state
+        /// </summary>
+        /// <param name="current">Current life evolution state</param>
+        /// <param name="horizontalIndex">Current horizontal dimension index</param>
+        /// <param name="verticalIndex">Current vertical dimension index</param>
+        /// <param name="directions">Allowed direction for neighbor search</param>
+        /// <returns>Evaluated state for next life evolution iteration</returns>
         private int Evaluate(ref int[,] current, ref int horizontalIndex, ref int verticalIndex, Directions directions)
         {
             var cell = current[horizontalIndex, verticalIndex];
@@ -36,6 +52,14 @@
             }
         }
 
+        /// <summary>
+        /// Collects neighbor cell life evolution state
+        /// </summary>
+        /// <param name="current">Current life evolution state</param>
+        /// <param name="horizontalIndex">Current horizontal dimension index</param>
+        /// <param name="verticalIndex">Current vertical dimension index</param>
+        /// <param name="directions">Allowed direction for neighbor search</param>
+        /// <returns>Count of live neighbor cells</returns>
         private int GetAliveNeighborCount(ref int[,] current, ref int horizontalIndex, ref int verticalIndex, ref Directions directions)
         {
             var aliveNeighbourCount = 0;
@@ -83,6 +107,14 @@
             return aliveNeighbourCount;
         }
 
+        /// <summary>
+        /// Returns available direction to neightbor cells 
+        /// </summary>
+        /// <param name="horizontalIndex">Current horizontal dimension index</param>
+        /// <param name="verticalIndex">Current vertical dimension index</param>
+        /// <param name="horizontalMax">Maximum horizontal dimension index</param>
+        /// <param name="verticalMax">Maximum vertical dimension index</param>
+        /// <returns>Flagged <see cref="Directions"/> with available direction neighbor cell are located</returns>
         private Directions GetAvailableDirections(int horizontalIndex, int verticalIndex, int horizontalMax, int verticalMax)
         {
             var directions = Directions.Left | Directions.Top | Directions.Right | Directions.Bottom;
