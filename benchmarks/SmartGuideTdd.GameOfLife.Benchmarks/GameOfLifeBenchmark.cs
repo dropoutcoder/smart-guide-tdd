ec1e685efe3636a65ad33af3f77a023987513cef
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-27 12:59:11 +0200

    Added GameofLifeBenchmark benchmark project

---
+++ benchmarks/SmartGuideTdd.GameOfLife.Benchmarks/GameOfLifeBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using SmartGuideTdd.GameOfLife;
+
+namespace SmartGuideTdd.FirstNonConsecutiveNumber.Benchmarks
+{
+    [MinColumn, MaxColumn, MemoryDiagnoser]
+    public class GameOfLifeBenchmark
+    {
+        public IEnumerable<int[,]> Values { get; }
+
+        private readonly GameOfLifeExecutor StandardExecution = new GameOfLifeExecutor(false);
+
+        private readonly GameOfLifeExecutor ParallelExecution = new GameOfLifeExecutor(true);
+
+        public GameOfLifeBenchmark()
+        {
+            Values = new[]
+            {
+                new int[,] { { 0, 1, 1, 0 }, { 1, 0, 0, 0 } },
+                new int[,] { { 0, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } },
+                new int[,] { { 0, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } },
+                new int[,] { { 1, 0, 0, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 0, 0, 1 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }  },
+            };
+        }
+
+        [ParamsSource(nameof(Values))]
+        public int[,] GameState { get; set; }
+
+        [Benchmark]
+        public int[,] Normal() => StandardExecution.Tick(GameState);
+
+        [Benchmark]
+        public int[,] Parallel() => ParallelExecution.Tick(GameState);
+    }
+}
---
+++ benchmarks/SmartGuideTdd.GameOfLife.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using SmartGuideTdd.FirstNonConsecutiveNumber.Benchmarks;
+
+namespace SmartGuideTdd.GameOfLife.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner
+                .Run<GameOfLifeBenchmark>();
+        }
+    }
+}
